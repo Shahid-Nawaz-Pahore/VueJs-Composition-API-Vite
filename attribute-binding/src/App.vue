52865cf7f19ebd204b6a9e8265186e71c109ab66
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-25 10:04:21 +0500

    attribute binding

---
+++ attribute-binding/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+const id="header"
+const clas='header'
+const type = 'text'
+const name='input'
+const value='value'
+const placeholder='enter input'
+const multipleAttributes= {
+       id:"header",
+        clas:'header',
+        type : 'text',
+        name:'input',
+        value:'value',
+        placeholder:'enter input'
+
+}
+const href="https://www.youtube.com/"
+const url="href"
+</script>
+
+<template>
+  <div class="">
+    <h1>binding attributes</h1>
+    <input :id="id" :name="name" :class="clas" :type="type" :value="value" :placeholder="placeholder">
+     <h1>binding multiple attributes dynimically </h1>
+     <input :="multipleAttributes"><br>
+     <a :[url]="href">youtube</a>
+  </div>
+  
+</template>
