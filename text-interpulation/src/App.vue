43920881aa762d594b8acd7e287413f1c11265ea
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-25 08:55:07 +0500

    text interpulation

---
+++ text-interpulation/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+const x =+prompt('Enter Number 1:');
+const y=+prompt('enter number 2:');
+
+function add(){
+  return x+y
+}
+</script>
+<template>
+  
+<div>
+  <h1>{{ x }}</h1> 
+  <h1>{{ y }}</h1>
+  <h1>{{add()%2==0? `${x}  and ${y}equal to: ${x+y} is even Number`:`${x} and ${y} is not equal to : ${x+y} is odd number` }}</h1>
+</div>
+</template>
