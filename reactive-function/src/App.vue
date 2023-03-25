61b72b5bf42bc7c3747791cbed4789b3661b5b09
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-25 11:27:09 +0500

    example 1 of reactive function

---
+++ reactive-function/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { reactive} from 'vue'
+let number = reactive({
+  a:22,
+  b:39
+})
+setTimeout(()=>{
+  console.log('timeout function is called')
+  number.a=98,
+  number.b=878,
+  console.log(typeof number)
+  console.log(`value of ${number.a} and ${number.b}`)
+},2000)
+</script>
+
+
+<template>
+ <h1>{{ number.a+number.b }}</h1>
+</template>
