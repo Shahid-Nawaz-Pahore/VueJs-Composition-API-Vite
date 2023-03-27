59629ec4d06ef543d09bd4272af82e18e2e2e025
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-28 00:53:01 +0500

    mathed in vuejs

---
+++ method-and-computed-property/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+let fName=ref('shahid')
+let lName=ref('nawaz')
+let age=ref(23)
+
+
+function display(){
+  return  `my name is ${fName.value} ${lName.value} and my age is ${age.value}`
+  
+}
+setTimeout(()=>{
+  console.log('change the values')
+  fName.value='zahid'
+  lName.value='amir'
+  age.value=30
+},2000)
+</script>
+
+<template>
+  <div>
+    <h1>who i am ?</h1><hr><hr>
+  <h1>{{ display() }}</h1>
+  </div>
+</template>
