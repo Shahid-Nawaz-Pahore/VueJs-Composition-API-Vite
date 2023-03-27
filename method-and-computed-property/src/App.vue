0f3237d8cf28b88ca3ca859ddec6c94691ac7218
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-28 01:26:48 +0500

    computed property in vuejs

--- method-and-computed-property/src/App.vue
+++ method-and-computed-property/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue'
+import {ref , computed} from 'vue'
 let fName=ref('shahid')
 let lName=ref('nawaz')
 let age=ref(23)
@@ -15,11 +15,27 @@ setTimeout(()=>{
   lName.value='amir'
   age.value=30
 },2000)
+
+//computed property
+let x=ref(10)
+let y=ref(30)
+const sum=computed(()=>{
+  return x.value+y.value
+})
+setTimeout(()=>{
+  x.value=200
+  y.value=87338
+},2000)
+
 </script>
 
 <template>
   <div>
-    <h1>who i am ?</h1><hr><hr>
-  <h1>{{ display() }}</h1>
+    <h1>Method in vuejs</h1>
+      <h1>{{ display() }}</h1>
+      <hr><hr>
+      <h1>computed property in vuejs</h1>
+      <h1>{{ sum }}</h1>
+      <p>computed property is fast then method when states are not changing  becase it create cashe </p>
   </div>
 </template>
