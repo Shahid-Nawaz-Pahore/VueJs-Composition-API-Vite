7a812b9370d4f82b1527656c1bfacfbe0648adc5
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-28 11:34:16 +0500

    render array using  v-for

---
+++ list-rendering-v-for/src/App.vue
@@ -0,0 +1,10 @@
+<script setup> 
+import {ref} from 'vue'
+const students=ref(['amir','kamran','zahid','shahid'])
+</script>
+
+<template>
+  <div>
+    <h1 v-for="(student,index) in students" :key="index">{{ index }} {{ student }}</h1>
+  </div>
+</template>
