8aadabc7f52edf2064a7419dca65af3f6373020a
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-28 10:15:50 +0500

    binding classes

---
+++ binding-html-class-attribute/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref , reactive , computed} from 'vue'
+const isActive = ref(true)
+const isImportant=ref(true)
+const obj=reactive({
+  active:true,
+  'f-bold':true,
+  fontSize:true
+})
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red f-bolder">hello</h1>
+    <h1 v-bind:class="{'active':isActive ,'f-bold':isImportant}">assign class using ref</h1>
+    <h1 v-bind:class="obj">assign class using reactive object</h1>
+    <h1 :class="isActive?'active':'notActive'">kjd</h1>
+</div>
+</template>
