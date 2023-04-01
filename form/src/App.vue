b86e1f209e73a5a6bb0cfb620c848b659872b2f7
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-04-01 11:51:05 +0500

    vue form all concept are apply about form

---
+++ form/src/App.vue
@@ -0,0 +1,87 @@
+<script setup>
+import {reactive} from 'vue'
+const formData=reactive({
+  name:'shahid',
+  email:'shahid@gmail.com',
+  password:'password',
+  message:'message',
+  hobies:[],
+  gender:'',
+  jobpost:'',
+  selectLocation:[],
+  fee:'',
+})
+ const states=reactive([
+  {text:'panjab',value:'panjab'},
+  {text:'lahore',value:'lahore'},
+  {text:'sindh',value:'sindh'},
+    {text:'islamabad',value:'islamabad'},
+    ]
+ )
+function formHandle(){
+  console.log(formData)
+}
+</script>
+
+<template>
+  
+  <form @submit.prevent="formHandle">
+    <pre>{{ formData}}</pre>
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" id="name" v-model.trim="formData.name">
+  </div>
+  <div>
+    <label for="email">Email:</label>
+    <input type="email" id="email" v-model="formData.email">
+  </div>
+  <div>
+    <label for="password">password:</label>
+    <input type="password" id="password" v-model="formData.password">
+  </div>
+  <div>
+    <label for="message">message:</label>
+    <textarea  id="message" v-model="formData.message"></textarea>
+  </div>
+  <div>
+    <span>Hobbies:</span>
+    <label for="reading">reading</label>
+    <input type="checkbox" id="reading" value="reading"  v-model="formData.hobies">
+    <label for="cooking">cooking</label>
+    <input type="checkbox" id="cooking" value="cooking"  v-model="formData.hobies">
+    <label for="listing">listing</label>
+    <input type="checkbox" id="listing" value="listing"  v-model="formData.hobies">
+    <label for="watching">watching</label>
+    <input type="checkbox" id="watching" value="watching"  v-model="formData.hobies">
+  </div>
+  <div>
+    <span>gender:</span>
+    <label for="male">Male</label>
+    <input type="radio" id="male" value="male" v-model="formData.gender">
+    <label for="female">female</label>
+    <input type="radio" id="female" value="female" v-model="formData.gender">
+    <label for="other">other</label>
+    <input type="radio" id="other" value="other" v-model="formData.gender">
+  </div>
+  <div>
+    <span>Job Post</span>
+    <select v-model="formData.jobpost">
+    <option disabled  value=""> Select job post</option>
+    <option  > laravel developer</option>
+    <option  > Vue developer</option>
+    <option  > angular developer</option>
+    </select>
+  </div>
+  <div>
+    <span>Select Job Lacation</span>
+    <select v-model="formData.selectLocation">
+    <option v-for="(state,index) in states" :key="index" :value="state.value">{{ state.text }}</option>
+    </select>
+  </div>
+  <div>
+    <label for="fees">Fees</label>
+    <input type="text" id="fees" v-model.number="formData.fee">
+  </div>
+  <button type="submit">Submit</button>
+</form>
+</template>
--- list-rendering-v-for/src/App.vue
+++ list-rendering-v-for/src/App.vue
@@ -1,10 +1,29 @@
 <script setup> 
 import {ref} from 'vue'
-const students=ref(['amir','kamran','zahid','shahid'])
+//array
+//const students=ref(['amir','kamran','zahid','shahid'])
+
+// object in array
+// const students=ref([
+//   {name:'shahid nawaz',roll:10,age:23},
+//   {name:'zahid nawaz',roll:11,age:20},
+//   {name:'amir nawaz',roll:18,age:25},
+//   {name:'kamran nawaz',roll:19,age:13}
+// ])
+
+const arr=ref([
+  [1,2,3,4],
+  [a,b,c,d]
+])
 </script>
 
 <template>
   <div>
-    <h1 v-for="(student,index) in students" :key="index">{{ index }} {{ student }}</h1>
+    <!-- array -->
+    <!-- <h1 v-for="(student,index) in students" :key="index">{{ index }} {{ student }}</h1> -->
+    <!-- objects in array -->
+  <!-- <h1 v-for="(student, index) in students"  :key="index">{{ index }} {{ `Name:${student.name} Roll#:${student.roll} age:${student.age} ` }}</h1> -->
+
+
   </div>
 </template>
