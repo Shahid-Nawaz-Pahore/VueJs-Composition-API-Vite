4207ef7a8d93b38ffbf4b2f78711189344aa8ce3
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-04-03 03:01:49 +0500

    pass props , declare props and and using props

---
+++ props/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<script setup>
+const fullName = defineProps(['firstName', 'lastName'])
+</script>
+
+<template>
+  <div class="about">
+
+    <pre>
+      <h1>{{ fullName }}</h1>
+      <h1>{{ firstName }}</h1>
+      <h1>{{ lastName }}</h1>
+    </pre>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ props/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+import {ref} from 'vue'
+import About from './AboutView.vue'
+
+</script>
+
+<template>
+  <main>
+
+    
+    <h1>Home page</h1>
+ 
+  </main>
+</template>
